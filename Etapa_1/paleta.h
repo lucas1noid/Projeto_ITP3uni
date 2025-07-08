83d7e535d43919ed9a14fa51d6e83ba8ced97f2b
Pedro Guzzo <pedroaguzzo1704@gmail.com>
2025-07-08 10:30:43 -0300

    Update paleta.h

--- Etapa_1/paleta.h
+++ Etapa_1/paleta.h
@@ -1,1 +1,22 @@
+#ifndef _Paleta
+#define _Paleta
 
+#define MAX 100
+
+struct Cor
+{
+    int R, G, B;
+};
+
+class Paleta
+{
+private:
+    int quantidade;
+    Cor cores[MAX];
+    double alturas[MAX];
+public:
+    void get_paleta(int qtd, Cor a[], double valores[]);
+    int ler_arquivo(const char* nome_arquivo, Paleta *paleta);
+};
+
+#endif
