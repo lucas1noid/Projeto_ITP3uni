d63c7def291b833e07c7d0b593b5ded7c564abcb
Lucas Lopes <lucas1noid@gmail.com>
2025-07-08 10:34:28 -0300

    Update Imagem.h
    declaração do .h

--- Etapa_2/Imagem.h
+++ Etapa_2/Imagem.h
@@ -1,1 +1,22 @@
+#ifndef _Imagem
+#define _Imagem
 
+class Imagem
+{
+private:
+    /* data */
+public:
+    Imagem(/* args */);
+    ~Imagem();
+};
+
+Imagem::Imagem(/* args */)
+{
+}
+
+Imagem::~Imagem()
+{
+}
+
+
+#endif
