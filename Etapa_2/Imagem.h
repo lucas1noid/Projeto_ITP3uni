1643b4ac28aa0c96442fc51658a587359227d421
Lucas Lopes <lucas1noid@gmail.com>
2025-07-10 09:34:39 -0300

    Update Imagem.h

--- Etapa_2/Imagem.h
+++ Etapa_2/Imagem.h
@@ -1,22 +1,20 @@
 #ifndef _Imagem
 #define _Imagem
+#include "../Etapa_1/paleta.h"
+
 
 class Imagem
 {
 private:
-    /* data */
+    int largura;
+    int altura;
+    Cor pixel[MAX][MAX];
+
 public:
-    Imagem(/* args */);
-    ~Imagem();
+    Imagem get_Imagem(int Clargura, int Caltura, Cor Cpixel[MAX][MAX]);
+    Imagem pixel_Imagem(int Clargura, int Caltura, Cor Cpixel[MAX][MAX]);
+    Imagem change_pixel(int Clargura, int Caltura, Cor Cpixel[MAX][MAX]);
+    Imagem save_Image(int Clargura, int Caltura, Cor Cpixel[MAX][MAX]);
 };
-
-Imagem::Imagem(/* args */)
-{
-}
-
-Imagem::~Imagem()
-{
-}
-
 
 #endif
