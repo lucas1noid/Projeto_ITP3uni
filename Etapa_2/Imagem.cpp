d4c7878cb710e08c1fc7221452cbd02f9f6de01d
Lucas Lopes <lucas1noid@gmail.com>
2025-07-10 10:28:35 -0300

    Update Imagem.cpp

--- Etapa_2/Imagem.cpp
+++ Etapa_2/Imagem.cpp
@@ -2,10 +2,9 @@
 #include "paleta.h"
 #include "Imagem.h"
 #include <fstream>
-
 using namespace std;
 
-void Imagem::save_Imagem(const char* nomeArquivo, int largura, int altura, Cor** matrizPixels) {
+    void Imagem::save_Image(const char* nomeArquivo, int largura, int altura, Cor** matrizPixels) {
     ofstream arquivo(nomeArquivo);
 
     if (!arquivo) {
@@ -23,7 +22,8 @@ void Imagem::save_Imagem(const char* nomeArquivo, int largura, int altura, Cor**
         for (int j = 0; j < largura; ++j) {
             arquivo << matrizPixels[i][j].R << " "
                     << matrizPixels[i][j].G << " "
-                    << matrizPixels[i][j].B << " ";
+                    << matrizPixels[i][j].B
+                     << " ";
         }
         arquivo << "\n";
     }
@@ -31,15 +31,23 @@ void Imagem::save_Imagem(const char* nomeArquivo, int largura, int altura, Cor**
     arquivo.close();
     std::cout << "Arquivo " << nomeArquivo << " salvo com sucesso.\n";
 }
-Imagem::get_Imagem(int Clargura, int Caltura, Cor Cpixel[1000][1000]){
+     void Imagem::get_Imagem(int Clargura, int Caltura, Cor Cpixel[MAX][MAX]){
         largura = Clargura;
         altura = Caltura;
         for (int i = 0; i < largura; i++)
     {
         for (int j = 0; j < altura; j++)
         {
-            pixel[i][j] = Cpixel[i][j];
+            pixel[i][j].R =;
         }
         
-    }
-};
+    };
+}
+    void Imagem::pixel_Imagem(int Clargura, int Caltura, Cor Cpixel[MAX][MAX]){
+       cout << Cpixel[Clargura][Caltura].R << " ";
+       cout << Cpixel[Clargura][Caltura].G << " ";
+       cout << Cpixel[Clargura][Caltura].B << endl;
+    };
+    Cor Imagem::change_pixel (int Clargura, int Caltura, double alturas[MAX]){
+        return 0;
+    };
