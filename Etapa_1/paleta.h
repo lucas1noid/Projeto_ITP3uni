8f0ad497acbdf7b0965bf67bf0b968d738f2e2d0
Pedro Guzzo <pedroaguzzo1704@gmail.com>
2025-07-11 10:32:00 -0300

    Update paleta.h

--- Etapa_1/paleta.h
+++ Etapa_1/paleta.h
@@ -15,8 +15,10 @@ private:
     Cor cores[MAX];
     double alturas[MAX];
 public:
+    Paleta();
     void get_paleta(int qtd, Cor a[], double valores[]);
     int ler_arquivo(const char* nome_arquivo, Paleta *paleta);
+    int get_color_by_height();
 };
 
 #endif
