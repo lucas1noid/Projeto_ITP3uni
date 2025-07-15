c1364cdb363a7bc7d4b57e7b696fe098770923f3
Lucas Lopes <lucas1noid@gmail.com>
2025-07-15 09:03:46 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include "Etapa_1/paleta.cpp"
+#include "Etapa_2/Imagem.cpp"
+#include "Etapa_3/Mapa.cpp"
+using namespace std;
+
+int main () {
+
+
+  return 0;
+}
