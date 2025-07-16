65f528dd9f56c6a01d5e1c6617ef91fa63139c14
Lucas Lopes <lucas1noid@gmail.com>
2025-07-16 19:00:23 -0300

    update bruto no mapa.h

--- Etapa_3/Mapa.h
+++ Etapa_3/Mapa.h
@@ -1,15 +1,31 @@
-#ifndef _Mapa
-#define _Mapa
-#include "Imagem.h"
-#include "paleta.h"
+#ifndef __Mapa
+#define __Mapa
+#include <string>
 
-class Mapa
-{
+class Mapa {
 private:
-    int terreno[][];
+    int tamanho;
+    float** matriz;
+
+    float deslocamentoAleatorio(float offset);
+    void diamond(int passo, float offset);
+    void square(int passo, float offset);
+    
+    void inicializarMatriz();
+    void desalocarMatriz();
 
 public:
-    Mapa(/* args */);
+    Mapa(int N);
+    ~Mapa();
+
+    void gerar(float rugosidade);
+    float consultar(int lin, int col) const;
+    int linhas() const;
+    int colunas() const;
+
+    void salvarEmArquivo(const std::string& nome) const;
+    void lerDeArquivo(const std::string& nome);
+    void imprimir() const;
 };
 
 #endif
