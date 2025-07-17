d29b8df644ddb28c42597dfd12cf323603485ed2
Lucas Lopes <lucas1noid@gmail.com>
2025-07-17 09:38:02 -0300

    mudança dos .cpp para .h

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include "Etapa_1/paleta.cpp"
-#include "Etapa_2/Imagem.cpp"
-#include "Etapa_3/Mapa.cpp"
+#include "Etapa_1/paleta.h"
+#include "Etapa_2/Imagem.h"
+#include "Etapa_3/Mapa.h"
 using namespace std;
 
 int main () {
