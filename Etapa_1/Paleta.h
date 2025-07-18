9df9baae85998d2aa51a6a3d25d75829f655a35d
Lucas Lopes <lucas1noid@gmail.com>
2025-07-17 23:48:02 -0300

    inicio das comentacoes

--- Etapa_1/Paleta.h
+++ Etapa_1/Paleta.h
@@ -1,8 +1,14 @@
 #ifndef _Paleta
 #define _Paleta
 
+/**
+Valor máximo definido apenas para melhor vizualização
+*/
 #define MAX 100
 
+/** 
+Salva toda cor como uma struct formada por 3 ints, representando respectivamente o RGB.
+*/
 struct Cor
 {
     int R, G, B;
@@ -15,9 +21,30 @@ private:
     Cor cores[MAX];
     double alturas[MAX];
 public:
+    
+    /**
+    Construtor, constroe a função com parametros base caso o usuário não inicialize-os.
+    @param nenhum
+    @return nenhum
+    */
     Paleta();
+    
+    /**
+    
+    */
     void get_paleta(int qtd, Cor a[], double valores[]);
+    
+    /**
+    
+    
+    */
     int ler_arquivo(const char* nome_arquivo, Paleta *paleta);
+    
+    /**
+    Consulta a cor que é correspondente à altura recebida.
+    @param altura que vai ser usada para encontrar sua respectiva cor na paleta.cor.
+    @return uma estrutura Cor, onde é retornado os valores R G B de uma determinada altura.
+    */
     Cor consultar_cor(double altura);
 };
 
