00ebf0a5b841d181371ef12b403fa7c1bfb222ec
Lucas Lopes <lucas1noid@gmail.com>
2025-07-17 23:59:47 -0300

    comentação do Imagem.h

--- Etapa_2/Imagem.h
+++ Etapa_2/Imagem.h
@@ -11,12 +11,38 @@ private:
     Cor** pixel;
 
 public:
+    /**
+    Construtor para criar uma imagem preta
+    @param int l (largura da imagem), int a (altura da imagem)
+    @return nenhum
+    */
     Imagem(int l, int a);
+    /**
+    Destrutor para limpar pixels da imagem 
+    @param nenhum
+    @return nenhum
+    */
     ~Imagem();
-    
-    Cor get_pixel(int linha, int coluna);
-    void change_pixel(int linha, int coluna, Cor cor);
+    /**
+    salva os dados de uma imagem no formato PPM(P3)
+    @param string nomeArquivo .ppm a ser salvo
+    @return nenhum
+    */
     void save_Image(std::string nomeArquivo);
+    
+    /**
+    Consultar um pixel qualquer na imagem
+    @param linha que se encontra o pixel, coluna onde se encontra o pixel
+    @return um pixel Tipo Cor
+    */
+    Cor get_pixel(int linha, int coluna);
+    
+    /**
+    mudar apenas 1 pixel na imagem
+    @param linha onde se encontra o pixel, coluna onde se encontra o pixel e a cor que o usuário deseja inserir.
+    @return nenhum
+    */
+    void change_pixel(int linha, int coluna, Cor cor);
 };
 
 #endif
